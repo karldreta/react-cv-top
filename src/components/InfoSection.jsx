bfe64cdee3d9e0f3dd9b3cf0c1d4f35195145492
Karl James <karldreta@gmail.com>
2026-02-19 14:14:54 +0800

    Add svg icons, Style InfoSection

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -28,7 +28,7 @@ export default function InfoSection() {
       <section className="sectLvl1">
         <div>
           {isEditing ? (
-            <>
+            <div className="nameDiv">
               <input
                 id="fullName"
                 value={profile.fullName}
@@ -39,55 +39,61 @@ export default function InfoSection() {
                 value={profile.title}
                 onChange={(e) => updateField("title", e.target.value)}
               />
-            </>
+            </div>
           ) : (
-            <>
+            <div className="nameDiv">
               <h1>{profile.fullName}</h1>
               <h2>{profile.title}</h2>
-            </>
+            </div>
           )}
         </div>
-        <div>
+        <div className="contactsDiv">
           <ul>
             {isEditing ? (
-              <li>
+              <li className="phone">
                 <input
                   value={profile.contactNum}
                   onChange={(e) => updateField("contactNum", e.target.value)}
                 />
               </li>
             ) : (
-              <li>{profile.contactNum}</li>
+              <li className="phone">
+                {profile.contactNum}
+              </li>
             )}
             {isEditing ? (
-              <li>
+              <li className="email">
                 <input
                   value={profile.emailAdd}
                   onChange={(e) => updateField("emailAdd", e.target.value)}
                 />
               </li>
             ) : (
-              <li>{profile.emailAdd}</li>
+              <li className="email">{profile.emailAdd}</li>
             )}
             {isEditing ? (
-              <li>
+              <li className="gitHub">
                 <input
                   value={profile.ghProfile}
                   onChange={(e) => updateField("ghProfile", e.target.value)}
                 />
               </li>
             ) : (
-              <li>{profile.ghProfile}</li>
+              <li className="gitHub">
+                <a href="https://github.com/karldreta" target="blank">
+                {profile.ghProfile}
+                </a>
+                </li>
             )}
             {isEditing ? (
-              <li>
+              <li className="location">
                 <input
                   value={profile.location}
                   onChange={(e) => updateField("location", e.target.value)}
                 />
               </li>
             ) : (
-              <li>{profile.location}</li>
+              <li className="location">{profile.location}</li>
             )}
           </ul>
         </div>
